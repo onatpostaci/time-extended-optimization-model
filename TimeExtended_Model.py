33e07620552b4f5ed4d8e6932bcd307f7b85e442
onatpostaci <onatpostman0gmail.com>
2024-06-05 23:42:12 +0300

    Scenarios are removed

--- TimeExtended_Model.py
+++ TimeExtended_Model.py
@@ -1,61 +1,48 @@
 from gurobipy import Model, GRB, quicksum
 
-#Initialize the model
+# Initialize the model
 m = Model("Time_Extended_Evacuation_Model")
 
 #-----------------------Example dataset---------------------#
 
-
 #**********SETS*********#
 
-#Set of Nodes in the System
+# Set of Nodes in the System
 nodes = ['A', 'B', 'C', 'D']
 
-#Set of Arcs in the System
+# Set of Arcs in the System
 arcs = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'A')] 
 
-#Set of Time steps
+# Set of Time steps
 time_steps = range(3)  
 
-#Set of Scenarios
-scenarios = range(2) 
-
-#Set of Origins
+# Set of Origins
 origins = ['A', 'B']  
 
-#Set of Destinations/Shelters
+# Set of Destinations/Shelters
 destinations = ['C', 'D']  
 
-#Time-extended copies of Nodes
+# Time-extended copies of Nodes
 time_extended_origins = [(o, t) for o in origins for t in time_steps]
 
-#Time-extended copies of Destinations
+# Time-extended copies of Destinations
 time_extended_destinations = [(d, t) for d in destinations for t in time_steps]
 
-#Set of Resources
+# Set of Resources
 resources = ['R1', 'R2'] 
 
-#Set of Deployable Nodes
+# Set of Deployable Nodes
 deployable_nodes = nodes
-
-#Set of identical/indistinguishable scenarios by time t;
-indistinct_scenarios = {
-    0: [0, 1],  # At time step 0, scenarios 0 and 1 are indistinguishable
-    1: [0, 1],
-    2: [0, 1]
-}
-
-#**********SETS*********#
 
 #**********PARAMETERS*********#
 
-#Evacuation demand at origin o ∈ O
+# Evacuation demand at origin o ∈ O
 q_o = {
        'A': 20,
        'B': 30
        }
 
-#Capacity of arc a ∈ A
+# Capacity of arc a ∈ A
 q_a = {
        ('A', 'B'): 15, 
        ('B', 'C'): 30, 
@@ -63,11 +50,11 @@ q_a = {
        ('D', 'A'): 25,
        }  
 
-#Additional capacity if a resource is deployed
+# Additional capacity if a resource is deployed
 delta_q_a = {a: 50 for a in arcs} 
 
-#Risk level on arc a ∈ A at time t ∈ T in scenario s ∈ S (Risk level might vary by arc, time step, and scenario)
-c_a_t_s = {(a, t, s): 0.1*t + 0.05*s for a in arcs for t in time_steps for s in scenarios}
+# Risk level on arc a ∈ A at time t ∈ T (Risk level might vary by arc and time step)
+c_a_t = {(a, t): 0.1*t for a in arcs for t in time_steps}
 
 #**********PARAMETERS*********#
 
@@ -75,57 +62,52 @@ c_a_t_s = {(a, t, s): 0.1*t + 0.05*s for a in arcs for t in time_steps for s in 
 
 #---------------------Decision variables--------------------#
 
-#1 if origin o is assigned to destination d, 0 otherwise
+# 1 if origin o is assigned to destination d, 0 otherwise
 x_od = {}
 for o in origins:
     for d in destinations:
         var_name = f"x_{o}_{d}"
-        x_od[(o,d)] = m.addVar(vtype=GRB.BINARY, name=var_name)
+        x_od[(o, d)] = m.addVar(vtype=GRB.BINARY, name=var_name)
 
-
-
-#1 if resource p is deployed to location n at time t in scenario s, 0 otherwise
-z_nts = m.addVars(nodes, time_steps, scenarios, vtype=GRB.BINARY, name="z_nts")
-w_ots = m.addVars(origins, time_steps, scenarios, vtype=GRB.BINARY, name="w_ots")
-y_ots = m.addVars(origins, time_steps, scenarios, vtype=GRB.BINARY, name="y_ots")
-r_ats = m.addVars(arcs, time_steps, scenarios, vtype=GRB.BINARY, name="r_ats")
-f_ao = m.addVars(arcs, origins, scenarios, vtype=GRB.CONTINUOUS, name="f_ao")
+# 1 if resource p is deployed to location n at time t, 0 otherwise
+z_nt = m.addVars(nodes, time_steps, vtype=GRB.BINARY, name="z_nt")
+w_ot = m.addVars(origins, time_steps, vtype=GRB.BINARY, name="w_ot")
+y_ot = m.addVars(origins, time_steps, vtype=GRB.BINARY, name="y_ot")
+r_at = m.addVars(arcs, time_steps, vtype=GRB.BINARY, name="r_at")
+f_ao = m.addVars(arcs, origins, vtype=GRB.CONTINUOUS, name="f_ao")
 
 #---------------------Decision variables--------------------#
 
 # Parameters - initialize c_ao correctly
-c_ao = {(a, o, s): 0.1 * s for a in arcs for o in origins for s in scenarios}
+c_ao = {(a, o): 0.1 for a in arcs for o in origins}
 
-# Objective Function - Corrected access
-objective_expr = quicksum(c_ao[(a, o, s)] * f_ao[a, o, s] for a in arcs for o in origins for s in scenarios)
+# Objective Function
+objective_expr = quicksum(c_ao[(a, o)] * f_ao[a, o] for a in arcs for o in origins)
 m.setObjective(objective_expr, GRB.MINIMIZE)
 
 # Constraints
 # Flow conservation
 for n in nodes:
     for t in time_steps:
-        for s in scenarios:
-            m.addConstr(
-                quicksum(f_ao[(i, n), o, s] for i, n in arcs if (i, n) in arcs for o in origins) ==
-                quicksum(f_ao[(n, j), o, s] for n, j in arcs if (n, j) in arcs for o in origins), 
-                "flow_conservation_{}_{}_{}".format(n, t, s)
-            )
+        m.addConstr(
+            quicksum(f_ao[(i, n), o] for i, n in arcs if (i, n) in arcs for o in origins) ==
+            quicksum(f_ao[(n, j), o] for n, j in arcs if (n, j) in arcs for o in origins), 
+            "flow_conservation_{}_{}".format(n, t)
+        )
 
 # Capacity constraints
 for a in arcs:
     for t in time_steps:
-        for s in scenarios:
-            m.addConstr(
-                quicksum(f_ao[a, o, s] for o in origins) <= q_a[a] * (1 - r_ats[a, t, s]) + 
-                quicksum(delta_q_a[a] * z_nts[n, t, s] for n in nodes) + (q_a[a] + delta_q_a[a]) * r_ats[a, t, s],
-                "capacity_{}_{}_{}".format(a, t, s)
-            )
+        m.addConstr(
+            quicksum(f_ao[a, o] for o in origins) <= q_a[a] * (1 - r_at[a, t]) + 
+            quicksum(delta_q_a[a] * z_nt[n, t] for n in nodes) + (q_a[a] + delta_q_a[a]) * r_at[a, t],
+            "capacity_{}_{}".format(a, t)
+        )
 
 # Resource deployment limits
 for n in nodes:
     for t in time_steps:
-        for s in scenarios:
-            m.addConstr(z_nts[n, t, s] <= 1, "resource_limit_{}_{}_{}".format(n, t, s))
+        m.addConstr(z_nt[n, t] <= 1, "resource_limit_{}_{}".format(n, t))
 
 # Optimize the model
 m.optimize()
